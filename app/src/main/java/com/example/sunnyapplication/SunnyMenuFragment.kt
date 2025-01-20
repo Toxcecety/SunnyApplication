efa7a242be0a792fceeca4a7bf18c53259c9979a
Celiane <celianeallaire@gmail.com>
2025-01-20 20:15:06 +0100

    Added the first fragments and implemented one directional nav for now

---
+++ app/src/main/java/com/example/sunnyapplication/SunnyMenuFragment.kt
@@ -0,0 +1,28 @@
+package com.example.sunnyapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.sunnyapplication.databinding.FragmentSunnyMenuBinding
+
+class SunnyMenuFragment : Fragment() {
+    private lateinit var binding: FragmentSunnyMenuBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSunnyMenuBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.navMenuToAboutButton.setOnClickListener {
+            findNavController().navigate(R.id.action_sunnyMenuFragment_to_sunnyAboutFragment)
+        }
+    }
+}
