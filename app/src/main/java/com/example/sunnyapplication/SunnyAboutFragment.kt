3daae6ba2258dfece81f8b7078d3daf352713ee0
Celiane <celianeallaire@gmail.com>
2025-01-22 15:36:35 +0100

    New sunny about fragment (WIP)

--- app/src/main/java/com/example/sunnyapplication/SunnyAboutFragment.kt
+++ app/src/main/java/com/example/sunnyapplication/SunnyAboutFragment.kt
@@ -5,15 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.sunnyapplication.databinding.FragmentSunnyAboutBinding
+import com.example.sunnyapplication.databinding.FragmentSunnyMenuBinding
 
 
 class SunnyAboutFragment : Fragment() {
+    private lateinit var binding: FragmentSunnyAboutBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sunny_about, container, false)
+        binding = FragmentSunnyAboutBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.navAboutToMenuButton.setOnClickListener {
+            findNavController().navigate(R.id.action_sunnyAboutFragment_to_sunnyMenuFragment)
+        }
+    }
 }
